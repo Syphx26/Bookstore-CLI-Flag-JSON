b8eadb2fe5d2ae368b2f75a0f206d6de1c119a24
Samuel Bird <sam.bird04@gmail.com>
2025-09-20 12:26:31 +0100

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   main.go
     Added ability to seach for book by id or all

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
 // define json structure
@@ -17,16 +18,33 @@ type Book struct {
 	IMG    string  `json:"image_url"`
 }
 
+// define commands structure
+type commands struct {
+	get    string
+	add    bool
+	update bool
+	delete bool
+}
+
 func main() {
 	var input Book
+	var cmd commands
 
 	// flags
-	commandType := flag.String("command", "read", "type of command to run")
-	flag.StringVar(&input.ID, "ID", "1", "ID of book")
+	// cmd
+	flag.StringVar(&cmd.get, "get", "false,all", "get all books or get book by id")
+	flag.BoolVar(&cmd.add, "add", false, "add book")
+	flag.BoolVar(&cmd.update, "update", false, "update book by id")
+	flag.BoolVar(&cmd.delete, "delete", false, "delete book by id")
+
+	// book info
+	flag.StringVar(&input.ID, "id", "1", "ID of book")
+	flag.StringVar(&input.Title, "title", "default title", "title of book")
+	flag.StringVar(&input.Author, "author", "default author", "author of book")
+	flag.Float64Var(&input.Price, "price", 0.0, "price of book")
+	flag.StringVar(&input.IMG, "img", "", "image url of book")
 
 	flag.Parse()
-
-	println("Command Type:", *commandType)
 
 	// open json file define jsonFile as var define err as error
 	jsonFile, err := os.Open("books.json")
@@ -36,9 +54,6 @@ func main() {
 		fmt.Println(err)
 		err = nil
 	}
-
-	// if no error print message
-	fmt.Println("Successfully Opened books.json")
 
 	// read json file
 	byteValue, err := io.ReadAll(jsonFile)
@@ -57,9 +72,39 @@ func main() {
 	// unmarshal byteValue into books
 	// unmarshal is a function that turns json data into a go struct
 	json.Unmarshal(byteValue, &books)
-	fmt.Print(books)
 
 	// closes jsonFile
 	defer jsonFile.Close()
 
+	if cmd.get[0:4] != "false" {
+		if cmd.get[len(cmd.get)-3:len(cmd.get)] == "all" {
+			// get all books
+			for i := 0; i < len(books); i++ {
+				fmt.Println(books[i])
+			}
+		}
+
+		if cmd.get[len(cmd.get)-2:] == "id" {
+			// get book by id
+			idx, err := strconv.Atoi(input.ID)
+			if err != nil {
+				fmt.Println(err)
+				err = nil
+			} else {
+				found := false
+				for i, b := range books {
+					if b.ID == input.ID {
+						fmt.Println(b)
+						found = true
+						_ = i
+						break
+					}
+				}
+
+				if !found {
+					fmt.Println("Book with ID", idx, "not found")
+				}
+			}
+		}
+	}
 }
